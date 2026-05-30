bc34c6134821a51aac5c754f18a7f071d71aa096
Navaro Devos <164686546+Navaro1234@users.noreply.github.com>
2026-05-30 16:06:57 +0200

    ok

--- startcode/main.py
+++ startcode/main.py
@@ -11,6 +11,14 @@ pygame.display.set_caption('Snake')
 
 def game_lus():
     snake = Snake(breedte//2, hoogte//2)
+
+    game_over = False
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+
     snake.teken(venster)
 
     pygame.display.update()
