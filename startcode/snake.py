1f2ee06cf875f043fbbf554f64e2c8aef679a347
Navaro Devos <164686546+Navaro1234@users.noreply.github.com>
2026-05-30 15:52:41 +0200

    ok

--- startcode/snake.py
+++ startcode/snake.py
@@ -1,15 +1,16 @@
 import pygame
 
+kleur_slang = (0, 255, 0)
 veld_grootte = 20
-kleur_slang = ""
 
 class Snake:
-    def __init__(self):
-
-    def beweeg(self):
+    def __init__(self, startx, starty):
+        self.x = startx
+        self.y = starty
+        self.lijst_slang = [[startx, starty]]
+        self.lengte_slang = 1
+        self.x_verandering = 0
+        self.y_verandering = 0
 
     def teken(self, venster):
-
-    def is_buiten_veld(self, breedte, hoogte):
-
-    def raakt_zichzelf(self):
+        self.x_verandering = self.x
